cc591c660b1de9ecbc6ca8a41adc712827673874
Pedro Rodrigues <pedro.hrs22@gmail.com>
2025-07-04 21:14:03 +0100

    Alterando build para vercel

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/teste-pratico-caramulino/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
